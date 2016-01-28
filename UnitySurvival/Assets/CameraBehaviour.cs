c0b29360028baafb307877750afdc7ea3af2234d
Kay Gonschior <kay.gonschior@googlemail.com>
2016-01-28 12:15:14 +0100

    Add Camera Script (Does not work)

---
+++ UnitySurvival/Assets/CameraBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    int i = 0;
+    private Transform cameraRef;
+
+	// Use this for initialization
+	void Start () {
+        cameraRef = GameObject.Find("Main Camera").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        i++;
+        cameraRef.position.Set(i, 0, 0);
+	}
+}
