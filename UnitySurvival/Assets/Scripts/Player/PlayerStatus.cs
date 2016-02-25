eaf822360dfce339ede973fadbd202d199ba216f
Kay Gonschior <kay.gonschior@googlemail.com>
2016-02-25 09:58:26 +0100

    Player Hunger and Thirst working

--- UnitySurvival/Assets/Scripts/Player/PlayerStatus.cs
+++ UnitySurvival/Assets/Scripts/Player/PlayerStatus.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerStatus : MonoBehaviour
 {
+    private Vector3 startPosition;
+
     public uint HEALTH_MAX;
     public uint HEALTH_MIN;
     public uint HUNGER_MAX;
@@ -15,25 +17,57 @@ public class PlayerStatus : MonoBehaviour
     public int TEMPERATURE_MAX;
     public int TEMPERATURE_NORMAL;
 
-    uint health;
-    uint hunger;
-    uint thirst;
-    uint tiredness;
-    int temperature;
+    public float HUNGER_GAIN_PER_TIME;
+    public float THIRST_GAIN_PER_TIME;
 
-    // Use this for initialization
-    void Start()
+    float health;
+    float hunger;
+    float thirst;
+    float tiredness;
+    float temperature;
+
+    bool alive;
+
+    private void init_values()
     {
+        GetComponent<Transform>().position = startPosition;
+
         health = HEALTH_MAX;
         hunger = HUNGER_MIN;
         thirst = THIRST_MIN;
         tiredness = TIREDNESS_MIN;
         temperature = TEMPERATURE_NORMAL;
+
+        alive = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = GetComponent<Transform>().position;
+        init_values();
     }
 
     // Update is called once per frame
     void Update()
     {
+        hunger += (float)HUNGER_GAIN_PER_TIME * Time.deltaTime;
+        thirst += (float)THIRST_GAIN_PER_TIME * Time.deltaTime;
 
+        if (hunger > HUNGER_MAX)
+            die();
+        if (thirst > HUNGER_MAX)
+            die();
     }
+
+    private void die()
+    {
+        alive = false;
+    }
+
+    public void respawn()
+    {
+        init_values();
+    }
+
 }
