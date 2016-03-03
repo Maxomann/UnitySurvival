dcf691ca228ed3cfb402c909b7a35f0608cc8828
Kay Gonschior <kay.gonschior@googlemail.com>
2016-03-03 11:36:24 +0100

    Add ConsumableSpawn

---
+++ UnitySurvival/Assets/Scripts/ConsumableSpawner.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsumableSpawner : MonoBehaviour
+{
+    bool timerActive = false;
+    int timePassed = 0;//in milliseconds
+    public int TIME_INTERVALL_CHECK_FOR_SPAWN;//in milliseconds
+
+    GameObject spawnedObject = null;
+    Transform thisTransform;
+
+    public int SPAWN_IN_ONE_OF_HOW_MANY;
+
+    void spawnObject()
+    {
+        int randomNumber = Random.Range(0, 2);
+
+        GameObject obj = null;
+
+        switch (randomNumber)
+        {
+            case 0:
+                obj = (GameObject)Instantiate(Resources.Load("Food/PrefabCoconut"));
+                break;
+            case 1:
+                obj = (GameObject)Instantiate(Resources.Load("Food/PrefabPineapple"));
+                break;
+            case 2:
+                obj = (GameObject)Instantiate(Resources.Load("Food/PrefabWatermelon"));
+                break;
+            default:
+                Debug.Log("Switch randomNumber in spawnObject() error");
+                break;
+        }
+
+        obj.GetComponent<Transform>().position = thisTransform.position;
+        spawnedObject = obj;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        thisTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnedObject == null)
+        {
+            if (timerActive)
+            {
+                if (timePassed > TIME_INTERVALL_CHECK_FOR_SPAWN)
+                {
+                    int shouldSpawn = Random.Range(0, SPAWN_IN_ONE_OF_HOW_MANY);
+
+                    if (shouldSpawn == 0)
+                    {
+                        spawnObject();
+                    }
+                    timerActive = false;
+                    timePassed = 0;
+                }
+                else
+                {
+                    timePassed += (int)(Time.deltaTime * 1000.0);
+                }
+            }
+            else
+            {
+                timerActive = true;
+            }
+        }
+    }
+}
---
+++ UnitySurvival/Assets/Scripts/ZValueSorting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZValueSorting : MonoBehaviour {
+
+    private SpriteRenderer tempRend;
+
+    void Start()
+    {
+        tempRend = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        tempRend.sortingOrder = (int)Camera.main.WorldToScreenPoint(tempRend.bounds.min).y * -1;
+    }
+}
