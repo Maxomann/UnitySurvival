1d52c2c9370e48b02c5022d629129df1af32f4c2
mlgwan <theciwan@gmail.com>
2016-03-29 16:17:47 +0200

    Optimized the code
    Made the code more clean and understandable. Also made an illuminatable
    material, which allo ws for a full day and night cycle (once realistic
    colors are chosen). The only thing left to do is to change the
    background-meshes to actual objects so they can be assigned with the
    illuminable material.

--- UnitySurvival/Assets/Scripts/DayNightCycle.cs
+++ UnitySurvival/Assets/Scripts/DayNightCycle.cs
@@ -4,53 +4,91 @@ using System.Collections;
 public class DayNightCycle : MonoBehaviour
 {
 
-    [Header("Time Settings")]
-    public int dayNightLength = 240; //Total length of a Cycle in seconds
-    public float curTime = 0;
-    [Range(0, 1)]
-    public float duskTime = 0.75f; //After how much % of a Cycle dusk begins
-    [Range(0, 1)]
-    public float dawnTime = 0.25f; //After how much % of a Cycle dawn begins
-    public float transitionDurationSeconds = 10;
+    //Global Variables
+    public static DayNightCycle curController { get; private set; } // Public getters, private setters
+    public static int curLightMode { get; private set; }
+    public static int totalDayLength { get; private set; }
+    public static int[] timeList { get; private set; }
+    public static Color[] colorList { get; private set; }
+    public static float transitionSpeed { get; private set; }
+    public static float transitionSpeedDelay { get; private set; }
 
-    [Header("Camera Settings")]
-    public Camera cam;
-    public Color lightColor;
-    public Color darkColour;
+    //Variables for the Editor Inspector window
+    public float _transitionSpeed = 0.01f;                                  // How fast to move through the Linear interpolation (Lerp)
+    public float _transitionSpeedDelay = 0.05f;                             // How many seconds to wait before Lerping further
+    public int _totalDayLength = 40;                                                             // How many seconds are in the whole day cycle
+    public int[] _timeList = new int[] { 8, 16, 24, 32 };             // When to trigger the color switches
+    public Color[] _colorList = new Color[] { Color.red, Color.cyan, Color.magenta, Color.yellow, Color.blue }; // What color to become
+                                                                                                                // Since the first color is a default color, the color list has to be one size larger than the timelist
+                                                                                                                
 
-
-    private void Start()
+    void Start()
     {
+        if (DayNightCycle.curController == null)
+        { // Another script hasn't already activated the ambient light system
+            if (_timeList != null && _colorList != null && _totalDayLength > 0)
+            { // The game object is now properly set up
+                if (_colorList.Length != _timeList.Length + 1)
+                {
+                    Debug.LogError("Color list size must be one more than time list size! The first color is the default color, the first time is when to move to the second color!");
+                    return;
+                }
+                DayNightCycle.curController = this; // From here on the ambient lighting system is controlled
+                DayNightCycle.totalDayLength = _totalDayLength;
+                DayNightCycle.timeList = _timeList;
+                DayNightCycle.colorList = _colorList;
+                DayNightCycle.transitionSpeed = _transitionSpeed;
+                DayNightCycle.transitionSpeedDelay = _transitionSpeedDelay;
+                RenderSettings.ambientLight = _colorList[0]; // Set the default color
+            }
+        }
+    }
 
-        if (curTime / dayNightLength > dawnTime && curTime / dayNightLength < duskTime)
+    float lastTime; // The time of day on the last FixedUpdate
+    bool waitingForDay; // Are we waiting for the next day?
+    bool needsClerp; // Should we restart the process?
+    void FixedUpdate()
+    {
+        if (DayNightCycle.curController == this)
         {
-
-            Debug.Log("It is day");
-
-        }
-        else {
-
-            Debug.Log("It is night");
+            float curTime = Time.fixedTime % DayNightCycle.totalDayLength; // The current time of day is the number of seconds since start mod the total day length in seconds
+            if (lastTime > curTime)
+            { // The day has reset
+                DayNightCycle.curLightMode = 0;
+                needsClerp = true;
+                waitingForDay = false;
+            }
+            if (!waitingForDay)
+            { // We are still within the time list's bounds
+                if (curTime > DayNightCycle.timeList[DayNightCycle.curLightMode])
+                { // The current time of day is more than the switch event
+                    DayNightCycle.curLightMode++; // So switch to the next light mode
+                    needsClerp = true; // Then start the process
+                    if (DayNightCycle.curLightMode >= DayNightCycle.timeList.Length)
+                    { // We're going too far outside the time list
+                        waitingForDay = true; // Wait until the next day resets the counter
+                    }
+                }
+            }
+            if (needsClerp)
+            { // Something changed our color
+                needsClerp = false; // Do not restart the process again
+                StopCoroutine("CLerp"); // Stop the current transition
+                StartCoroutine("CLerp", colorList[curLightMode]); // Lerp to this now
+            }
+            lastTime = curTime; // For checking if the day has reset
         }
     }
 
-    private void Update()
+    IEnumerator CLerp(Color newColor)
     {
-
-        InvokeRepeating("DayNightTick", 1f, 1f);
-    }
-
-    private void DayNightTick()
-    {
-
-        if (curTime++ >= dayNightLength)
-        {
-
-            curTime = 0;
+       
+        Color oldColor = RenderSettings.ambientLight; // The current color
+        for (float t = 0; t < 1 + DayNightCycle.transitionSpeed; t += DayNightCycle.transitionSpeed)
+        { // We need to add one extra step
+            RenderSettings.ambientLight = Color.Lerp(oldColor, newColor, t); // Lerp the ambient light towards the new color
+            yield return new WaitForSeconds(DayNightCycle.transitionSpeedDelay); // Wait for the assigned number of seconds
         }
-
+       
     }
-
 }
-
-
